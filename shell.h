c038c9e57f1ff240488d1eb1114088ec1cdd09d9
somin254 <sominrenson@gmai.com>
2023-12-08 05:48:06 -0800

    shell.h

--- shell.h
+++ shell.h
@@ -6,7 +6,20 @@
 #include <unistd.h>
 
 /*--PROTOTYPES---*/
-/*main.c*/
+
+int changeCurrentDirectory(info_t *infos);
+int printCurrentEnvironment(info_t *infos);
+int initializeOrModifyEnvVar(info_t *infos);
+char *getEnvVarValue(info_t *infos, const char *names);
+int populateEnvList(info_t *info);
+int removeEnvVar(info_t *infos);
+int showHelp(info_t *infos);
+int exitShell(info_t *infos);
+/*errors.c*/
+void _eputs(char *str);
+int _putfd(char ch, int fd);
+int _putsfd(char *str, int fd);
+int _eputchar(char ch);
 
 
 
