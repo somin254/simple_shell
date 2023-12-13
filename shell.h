123eac6f31900bd35091d51cfebb99dd1a3898b6
somin254 <sominrenson@gmai.com>
2023-12-12 20:36:53 -0800

    shell.h

--- shell.h
+++ shell.h
@@ -1,12 +1,11 @@
 #ifndef SHELL_H
 #define SHELL_H
-
-/* Standard Library Headers */
+ 
+/*-----LIBRARIES---*/
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>  // Add any other standard library headers as needed
+#include <stdlib.h>
 
-/* Custom Structures */
 typedef struct list_t {
     char *str;
     struct list_t *next;
@@ -19,12 +18,14 @@ typedef struct {
     int status;
     int err_num;
     int readfd;
-    list_t *env_list;  // Renamed from env to env_list for clarity
-} info_t;
+    list_t *env_list;
+} 
+info_t;
 
 #define WRITE_BUF_SIZE 256
 
-/* Function Prototypes */
+/*--PROTOTYPES---*/
+
 int changeCurrentDirectory(info_t *infos);
 int printCurrentEnvironment(info_t *infos);
 int initializeOrModifyEnvVar(info_t *infos);
@@ -33,18 +34,17 @@ int populateEnvList(info_t *info);
 int removeEnvVar(info_t *infos);
 int showHelp(info_t *infos);
 int exitShell(info_t *infos);
-
-/* errors.c */
+/*errors.c*/
 void _eputs(char *str);
 int _putfd(char ch, int fd);
 int _putsfd(char *str, int fd);
 int _eputchar(char ch);
-
-/* _ATOI.C */
+/*_ATOI.C*/
 int interactive(info_t *infos);
 int _isalpha(int ch);
 int _atoi(char *str);
-int is_delim(char ch, char *delim);
+int is_delim(char ch, char *delime);
+
+
 
 #endif
-
