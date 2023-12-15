740564540b60d837431e05646d6fed1209da5856
somin254 <sominrenson@gmai.com>
2023-12-15 12:06:44 -0800

    shell.h

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define BUFFER_SIZE 1024
+
+void display_prompt(void);
+void execute_command(char *command);
+
+#endif /* SHELL_H */
