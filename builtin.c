d978d4046407761e8c5e70a34725a5bace640588
fordstile <masterland9772@gmail.com>
2023-12-15 11:01:57 -0800

    updated

--- builtin.c
+++ builtin.c
@@ -1,100 +1,98 @@
 #include "shell.h"
 
 /**
- * changeCurrentDirectory - Change the current directory of the process.
- * @infos: Struct containing arguments, used to maintain const func prototype.
- *
- * Return: Always (0)
+ * _myexit - Exits the shell
+ * @info: Structure containing potential arguments. Used to maintain
+ *         constant function prototype.
+ * Return: Exits with a given exit status
+ *         (0) if info->argv[0] != "exit"
  */
-int changeCurrentDirectory(info_t *infos)
+int _myexit(info_t *info)
 {
-        char *currentPath, *dir, buffer[1024];
-        int chdirResult;
+	int exit_check;
 
-        currentPath = getcwd(buffer, 1024);
-        if (!currentPath)
-                _eputs("TODO: >>getcwd failure emsg here<<\n");
-
-        if (!infos->argv[1])
-        {
-                dir = _getenv(infos, "HOME=");
-                if (!dir)
-                        chdirResult = chdir((dir = _getenv(infos, "PWD=")) ? dir : "/");
-                else
-                        chdirResult = chdir(dir);
-        }
-        else if (_strcmp(infos->argv[1], "-") == 0)
-        {
-                if (!_getenv(infos, "OLDPWD="))
-                {
-                        _puts(currentPath);
-                        _putchar('\n');
-                        return (1);
-                }
-                _puts(_getenv(infos, "OLDPWD=")), _putchar('\n');
-                chdirResult = chdir((dir = _getenv(infos, "OLDPWD=")) ? dir : "/");
-        }
-        else
-                chdirResult = chdir(infos->argv[1]);
-
-        if (chdirResult == -1)
-        {
-                print_error(infos, "can't cd to ");
-                _eputs(infos->argv[1]), _eputchar('\n');
-        }
-        else
-        {
-                _setenv(infos, "OLDPWD", _getenv(infos, "PWD="));
-                _setenv(infos, "PWD", getcwd(buffer, 1024));
-        }
-        return (0);
+	if (info->argv[1])  /* If there is an exit argument */
+	{
+		exit_check = _erratoi(info->argv[1]);
+		if (exit_check == -1)
+		{
+			info->status = 2;
+			print_error(info, "Illegal number: ");
+			_eputs(info->argv[1]);
+			_eputchar('\n');
+			return (1);
+		}
+		info->err_num = _erratoi(info->argv[1]);
+		return (-2);
+	}
+	info->err_num = -1;
+	return (-2);
 }
 
 /**
- * showHelp - Display help information.
- * @infos: Struct containing arguments, used to maintain const func prototype.
- *
- * Return: Always (0)
+ * _mycd - Changes the current directory of the process
+ * @info: Structure containing potential arguments. Used to maintain
+ *         constant function prototype.
+ * Return: Always 0
  */
-int showHelp(info_t *infos)
+int _mycd(info_t *info)
 {
-        char **argumentsArray;
+	char *s, *dir, buffer[1024];
+	int chdir_ret;
 
-        argumentsArray = infos->argv;
-        _puts("help call works. Function not yet implemented \n");
-
-        if (0)
-                _puts(*argumentsArray);
-
-        return (0);
+	s = getcwd(buffer, 1024);
+	if (!s)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
+	if (!info->argv[1])
+	{
+		dir = _getenv(info, "HOME=");
+		if (!dir)
+			chdir_ret = /* TODO: what should this be? */
+				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+		else
+			chdir_ret = chdir(dir);
+	}
+	else if (_strcmp(info->argv[1], "-") == 0)
+	{
+		if (!_getenv(info, "OLDPWD="))
+		{
+			_puts(s);
+			_putchar('\n');
+			return (1);
+		}
+		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+		chdir_ret = /* TODO: what should this be? */
+			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+	}
+	else
+		chdir_ret = chdir(info->argv[1]);
+	if (chdir_ret == -1)
+	{
+		print_error(info, "can't cd to ");
+		_eputs(info->argv[1]), _eputchar('\n');
+	}
+	else
+	{
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
 }
 
 /**
- * exitShell - Exit the shell.
- * @infos: Struct containing arguments, used to maintain const func prototype.
- *
- * Return: Exits if given an exit status. If infos.argv[0] != "exit" (0)
+ * _myhelp - Displays help information
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
  */
-int exitShell(info_t *infos)
+int _myhelp(info_t *info)
 {
-        int exitStatus;
+	char **arg_array;
 
-        if (infos->argv[1])
-        {
-                exitStatus = _erratoi(infos->argv[1]);
-                if (exitStatus == -1)
-                {
-                        infos->status = 2;
-                        print_error(infos, "Illegal number: ");
-                        _eputs(infos->argv[1]);
-                        _eputchar('\n');
-                        return (1);
-                }
-                infos->err_num = _erratoi(infos->argv[1]);
-                return (-2);
-        }
-
-        infos->err_num = -1;
-        return (-2);
+	arg_array = info->argv;
+	_puts("help call works. Function not yet implemented \n");
+	if (0)
+		_puts(*arg_array); /* temp att_unused workaround */
+	return (0);
 }
 
--- shell.h
+++ shell.h
@@ -1,12 +1,11 @@
 #ifndef CISFUN_SHELL_H
 #define CISFUN_SHELL_H
+
 #include <stdlib.h>
+
 #define WRITE_BUF_SIZE 256
 #define PROMPT "#cisfun$ "
 
-typedef struct list_t {
-    char *str;
-    struct list_t *next;
 /**
  * struct list_t - Definition of a linked list node
  * @str: String data
@@ -18,15 +17,6 @@ typedef struct list_t
 	struct list_t *next;     /* Pointer to the next node */
 } list_t;
 
-typedef struct {
-    char **argv;
-    char **env;
-    int argc;
-    int status;
-    int err_num;
-    int readfd;
-    list_t *env_list;
-    const char *prompt;
 /**
  * struct info_t - Definition of information struct for the shell
  * @argv: Argument vector
@@ -59,16 +49,19 @@ int populateEnvList(info_t *info);
 int removeEnvVar(info_t *infos);
 int showHelp(info_t *infos);
 int exitShell(info_t *infos);
+
 /* Errors */
 void _eputs(char *str);
 int _putfd(char ch, int fd);
 int _putsfd(char *str, int fd);
 int _eputchar(char ch);
+
 /* _ATOI.C */
 int interactive(info_t *infos);
 int _isalpha(int ch);
 int _atoi(char *str);
 int is_delim(char ch, char *delim);
+
 void _puts(const char *str);
 char *_getenv(info_t *infos, const char *name);
 int _strcmp(const char *s1, const char *s2);
@@ -76,4 +69,6 @@ void _putchar(char c);
 void print_error(info_t *infos, const char *message);
 void _setenv(info_t *infos, const char *name, const char *value);
 int _erratoi(const char *str);
+
 #endif /* CISFUN_SHELL_H */
+
