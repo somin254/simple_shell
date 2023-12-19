4eba470870e9269c6107996d3fc3473d87fbd06a
fordstile <masterland9772@gmail.com>
2023-12-19 05:59:29 -0800

    updated

--- buitIn.c
+++ buitIn.c
@@ -1,33 +1,33 @@
 #include "main.h"
 
 /**
- * _atoi - convert a string to an integer.
+ * custom_atoi - convert a string to an integer.
  * @s: char type string
  * Return: integer converted
  */
 
-int _atoi(char *s)
+int custom_atoi(char *s)
 {
-	int i;
-	int result = 0;
-	int sign = -1;
-	int brk = 0;
+    int i;
+    int result = 0;
+    int sign = -1;
+    int brk = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == '-')
-			sign = sign * -1;
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result = result * 10;
-			result -= (s[i] - '0');
-			brk = 1;
-		}
-		else if (brk == 1)
-			break;
-	}
-	result = sign * result;
-	return (result);
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        if (s[i] == '-')
+            sign = sign * -1;
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            result = result * 10;
+            result -= (s[i] - '0');
+            brk = 1;
+        }
+        else if (brk == 1)
+            break;
+    }
+    result = sign * result;
+    return (result);
 }
 
 /**
@@ -40,30 +40,30 @@ int _atoi(char *s)
 
 void execute_exit(char **array, char *command)
 {
-	int status;
+    int status;
 
-	if (array[1] != NULL)
-	{
-		status = _atoi(array[1]);
-		if (status == 0 && *array[1] != '0')
-		{
-			write(STDERR_FILENO, "./hsh: 1: exit: Illegal number: ", 34);
-			write(STDERR_FILENO, array[1], _strlen(array[0]));
-			write(STDERR_FILENO, "\n", 1);
-			free(command);
-			free(array);
-			exit(2);
-		}
-		free(command);
-		free(array);
-		exit(status);
-	}
-	else
-	{
-		free(command);
-		free(array);
-		exit(0);
-	}
+    if (array[1] != NULL)
+    {
+        status = custom_atoi(array[1]);
+        if (status == 0 && *array[1] != '0')
+        {
+            write(STDERR_FILENO, "./hsh: 1: exit: Illegal number: ", 34);
+            write(STDERR_FILENO, array[1], _strlen(array[0]));
+            write(STDERR_FILENO, "\n", 1);
+            free(command);
+            free(array);
+            exit(2);
+        }
+        free(command);
+        free(array);
+        exit(status);
+    }
+    else
+    {
+        free(command);
+        free(array);
+        exit(0);
+    }
 }
 
 /**
@@ -74,13 +74,14 @@ void execute_exit(char **array, char *command)
 
 void execute_env(void)
 {
-	int i = 0;
-	char **env = environ;
+    int i = 0;
+    char **env = environ;
 
-	while (env[i])
-	{
-		write(STDOUT_FILENO, (const void *)env[i], _strlen(env[i]));
-		write(STDOUT_FILENO, "\n", 1);
-		i++;
-	}
+    while (env[i])
+    {
+        write(STDOUT_FILENO, (const void *)env[i], _strlen(env[i]));
+        write(STDOUT_FILENO, "\n", 1);
+        i++;
+    }
 }
+
--- execute.c
+++ execute.c
@@ -10,43 +10,69 @@
 
 void execute(char *array[], char *const *environment)
 {
-	int status;
-	char *path, *token, command_path[1024];
-	struct stat st;
-	pid_t pid = fork();
+    int status;
+    char *path, *token;
+    char *command_path = NULL;
+    size_t path_length = 0;
 
-	if (pid == -1)
-	{
-		perror("Fork Failed!");
-		exit(EXIT_FAILURE); }
-	else if (pid == 0)
-	{
-		if (array[0][0] == '/' || array[0][0] == '.')
-		{
-			if (stat(array[0], &st) == 0 && S_ISREG(st.st_mode))
-			{
-				if (execve(array[0], array, environment) == -1)
-				{
-					perror("./shell");
-					exit(EXIT_FAILURE); }}}
-		else
-		{
-			path = _getenv("PATH");
-			token = _strtok(path, ":");
-			while (token != NULL)
-			{ _strcpy(command_path, token);
-				_strcat(command_path, "/");
-				_strcat(command_path, array[0]);
-				if (stat(command_path, &st) == 0 && S_ISREG(st.st_mode))
-				{
-					if (execve(command_path, array, environment) == -1)
-					{
-						perror("./shell");
-						exit(EXIT_FAILURE); }}
-				token = _strtok(NULL, ":"); }
-			write(STDERR_FILENO, "Command not found: ", 19);
-			write(STDERR_FILENO, array[0], _strlen(array[0]));
-			write(STDERR_FILENO, "\n", 1);
-			exit(EXIT_FAILURE); }}
-	else
-		waitpid(pid, &status, 0); }
+    struct stat st;
+    pid_t pid = fork();
+
+    if (pid == -1)
+    {
+        perror("Fork Failed!");
+        exit(EXIT_FAILURE);
+    }
+    else if (pid == 0)
+    {
+        if (array[0][0] == '/' || array[0][0] == '.')
+        {
+            if (stat(array[0], &st) == 0 && S_ISREG(st.st_mode))
+            {
+                if (execve(array[0], array, environment) == -1)
+                {
+                    perror("./shell");
+                    exit(EXIT_FAILURE);
+                }
+            }
+        }
+        else
+        {
+            path = _getenv("PATH");
+            token = _strtok(path, ":");
+            while (token != NULL)
+            {
+                path_length = _strlen(token) + 1; // +1 for the '/'
+                path_length += _strlen(array[0]) + 1; // +1 for the null terminator
+                command_path = malloc(path_length);
+                if (command_path == NULL)
+                {
+                    perror("Memory allocation failed");
+                    exit(EXIT_FAILURE);
+                }
+                _strcpy(command_path, token);
+                _strcat(command_path, "/");
+                _strcat(command_path, array[0]);
+                if (stat(command_path, &st) == 0 && S_ISREG(st.st_mode))
+                {
+                    if (execve(command_path, array, environment) == -1)
+                    {
+                        perror("./shell");
+                        exit(EXIT_FAILURE);
+                    }
+                }
+                free(command_path);
+                token = _strtok(NULL, ":");
+            }
+            write(STDERR_FILENO, "Command not found: ", 19);
+            write(STDERR_FILENO, array[0], _strlen(array[0]));
+            write(STDERR_FILENO, "\n", 1);
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        waitpid(pid, &status, 0);
+    }
+}
+
